037dcf3deb5a4f3a5c5b741eb8f6c3cd62c657fc
宋梦瑶 <3076117417@qq.com>
2019-08-17 23:22:48 +0800

    like express middleware principle

--- express-demo.js
+++ express-demo.js
@@ -1,5 +1,6 @@
 const http = require('http')
-const express = require('express')
+// const express = require('express')  // 官网的 express
+const express = require('./like-express/index')  // like express
 
 // 当前 http 请求的实例
 const app = express()
@@ -10,6 +11,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
+    console.log('假设处理cookie....')
     // 假设处理cookie
     req.cookie = {
         userId: '123abc'
@@ -21,6 +23,7 @@ app.use((req, res, next) => {
     // 假设处理 postdata
     // 异步处理
     setTimeout(() => {
+        console.log('假设处理 postdata....')
         req.body = {
             a: 100,
             b: 200
@@ -43,6 +46,14 @@ app.post('/api', (req, res, next) => {
     console.log('post /api路由')
     next()
 })
+
+// 模拟登陆验证
+function loginCheck () {
+    setTimeout(() => {
+        console.log('模拟登陆成功')
+        next()
+    })
+}
 
 app.get('/api/get-cookie', (req, res, next) => {
     console.log('get /api/get-cookie')
@@ -68,6 +79,6 @@ app.use((req, res, next) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000')
+app.listen(8000, () => {
+    console.log('server is running on port 8000')
 })
